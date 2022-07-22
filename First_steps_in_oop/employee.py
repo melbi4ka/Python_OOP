a1baf043af64af9492e3be9276a7d61bf078abbf
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-22 17:45:08 +0300

    Add files via upload

---
+++ First_steps_in_oop/employee.py
@@ -0,0 +1,28 @@
+class Employee:
+
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_annual_salary(self):
+        total_salary = self.salary * 12
+        return total_salary
+
+    def raise_salary(self, amounth):
+        self.salary += amounth
+        return self.salary
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
+
+
+
+
+
