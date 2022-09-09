acdadcf7dfb7f104fd4a07b8ea2b8085899534d4
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-09 18:14:04 +0300

    Add files via upload

---
+++ Inheritance/shop/project/product_repository.py
@@ -0,0 +1,34 @@
+from project.product import Product
+
+
+class ProductRepository:
+
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+
+    # def __repr__(self):
+    #     res = ""
+    #     for product in self.products:
+    #         res += f"{product.name}: {product.quantity}"
+    #         res += "\n"
+    #     return res.strip()
+
+    def __repr__(self):
+        res = ""
+        for product in self.products:
+            res += f"{product.name}: {product.quantity}\n"
+        return res.strip()
+
