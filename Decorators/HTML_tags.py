03da47a77cd926b1181cae7f49c9df541fae352f
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-17 14:40:58 +0300

    Update HTML_tags.py

--- Decorators/HTML_tags.py
+++ Decorators/HTML_tags.py
@@ -27,16 +27,3 @@ def to_upper(text):
     return text.upper()
 print(to_upper('hello'))
 
-
-# test first zero
-import unittest
-
-class TagsTests(unittest.TestCase):
-    def test_zero_first(self):
-        @tags('p')
-        def join_strings(*args):
-            return "".join(args)
-        self.assertEqual(join_strings("Hello", " you!"), '<p>Hello you!</p>')
-
-if __name__ == '__main__':
-    unittest.main()
