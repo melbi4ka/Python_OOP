e734a94e8e520645ca5cf7d6d529c2c1c912f19c
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-20 20:58:50 +0300

    Add files via upload

---
+++ Iterators_and_generators/possible_permutations.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+def possible_permutations(iterable):
+
+    for el in permutations(iterable):
+        yield list(el)
+
+
+
+[print(n) for n in possible_permutations([1, 2, 3])]
