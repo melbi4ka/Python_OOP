99f6c2508d63510bc9fd99c72bcae8dc38e62530
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-21 21:02:26 +0300

    Add files via upload

---
+++ Iterators_and_generators/take_halves.py
@@ -0,0 +1,28 @@
+def solution():
+    def integers():
+        num = 1
+        while True:
+            yield num
+            num += 1
+
+
+    def halves():
+        for i in integers():
+            yield i/2
+
+
+    def take(n, seq):
+        result = []
+        while n > 0:
+            result.append(next(seq))
+            n -= 1
+        return result
+
+
+    return (take, halves, integers)
+
+
+
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
