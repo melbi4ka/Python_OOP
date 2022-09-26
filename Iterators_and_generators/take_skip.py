a506d47bfb54cb8ade5375c724a553978604d1f0
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-26 21:07:47 +0300

    Add files via upload

---
+++ Iterators_and_generators/take_skip.py
@@ -0,0 +1,29 @@
+class take_skip:
+
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        while self.count > 0:
+            result = self.start
+            self.start += self.step
+            self.count -= 1
+            return result
+        raise StopIteration()
+
+# numbers = take_skip(2, 6)
+# for number in numbers:
+#     print(number)
+
+numbers = take_skip(10, 5)
+for number in numbers:
+    print(number)
+
+
+        
