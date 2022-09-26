65903c6ed91ac3620122a7ea31286dbc4c36e2e7
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-26 21:06:24 +0300

    Add files via upload

---
+++ Iterators_and_generators/take_skip_nasko.py
@@ -0,0 +1,28 @@
+class take_skip:
+
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.iteration = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        while self.count > 0:
+            if self.iteration == self.count:
+                raise StopIteration
+
+            result = self.iteration * self.step
+            self.iteration += 1
+            return result
+
+
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+
+numbers = take_skip(10, 5)
+for number in numbers:
+    print(number)
