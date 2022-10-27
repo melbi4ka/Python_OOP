ddb58d2a489483af34d1a28718315fe29b801e85
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-10-27 20:03:16 +0300

    Update groups.py

--- Polymorphism and abstraction/groups/groups.py
+++ Polymorphism and abstraction/groups/groups.py
@@ -45,38 +45,3 @@ print(third_group[0])
 
 for person in third_group:
     print(person)
-
-
-# p0 = Person('Aliko', 'Dangote')
-# p1 = Person('Bill', 'Gates')
-# p2 = Person('Warren', 'Buffet')
-# p3 = Person('Elon', 'Musk')
-# p4 = p2 + p3
-#
-# first_group = Group('__VIP__', [p0, p1, p2])
-# second_group = Group('Special', [p3, p4])
-# third_group = first_group + second_group
-#
-# # print(len(first_group))
-# # print(second_group)
-# # print(third_group)
-# print(len(first_group))
-# print(second_group)
-# print(third_group[0])
-#
-# for person in third_group:
-#     print(person)
-
-
-
-
-
-
-
-# p0 = Person('Aliko', 'Dangote')
-# p1 = Person('Bill', 'Gates')
-# p3 = p0 + p1
-# print(p0)
-# print(type(p1))
-# print(p3)
-# print(type(p3))
