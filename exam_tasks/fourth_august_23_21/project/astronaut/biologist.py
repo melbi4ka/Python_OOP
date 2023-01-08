8f7b0f33e0b1e353d8c5b20b19cd92580d5ca510
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2023-01-08 16:16:47 +0200

    Update biologist.py

--- exam_tasks/fourth_august_23_21/project/astronaut/biologist.py
+++ exam_tasks/fourth_august_23_21/project/astronaut/biologist.py
@@ -3,8 +3,6 @@ from project.astronaut.astronaut import Astronaut
 
 class Biologist(Astronaut):
 
-    # def __init__(self, name):
-    #     super().__init__(name, oxygen = 70)
 
     def __init__(self, name, oxygen = 70):
         super().__init__(name, oxygen)
