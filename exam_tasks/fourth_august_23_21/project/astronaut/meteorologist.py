fe90ec7cf9534383776a29068b71d7c8ae22232a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2023-01-09 14:52:49 +0200

    Update meteorologist.py

--- exam_tasks/fourth_august_23_21/project/astronaut/meteorologist.py
+++ exam_tasks/fourth_august_23_21/project/astronaut/meteorologist.py
@@ -3,9 +3,6 @@ from project.astronaut.astronaut import Astronaut
 
 class Meteorologist(Astronaut):
 
-    # def __init__(self, name):
-    #     super().__init__(name, oxygen = 90)
-
     def __init__(self, name, oxygen = 90):
         super().__init__(name, oxygen)
 
