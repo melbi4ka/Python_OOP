7d4c82d8837fefdb59dbf7a93dff811f619a754c
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2023-01-09 14:52:33 +0200

    Update geodesist.py

--- exam_tasks/fourth_august_23_21/project/astronaut/geodesist.py
+++ exam_tasks/fourth_august_23_21/project/astronaut/geodesist.py
@@ -3,18 +3,8 @@ from project.astronaut.astronaut import Astronaut
 
 class Geodesist(Astronaut):
 
-    # def __init__(self, name):
-    #     super().__init__(name, oxygen = 50)
-
     def __init__(self, name, oxygen = 50):
         super().__init__(name, oxygen)
 
     def breathe(self):
         self.oxygen -= 10
-
-#     def __str__(self):
-#         return f"{self.name}{self.oxygen}"
-#
-#
-# g = Geodesist("Neil")
-# print(g)
